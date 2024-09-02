243e6e3ff1394b5298c6d24a1aeeab050f90d8d5
Natanel Gabay <natanelg1@gmail.com>
2024-09-02 14:25:21 +0300

    chore: Add navigation options for user-specific actions in Footer.jsx

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
+import RecentActorsIcon from "@mui/icons-material/RecentActors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import StyleIcon from "@mui/icons-material/Style";
 import { useNavigate } from "react-router-dom";
 import ROUTES from "../../routes/routesModel";
+import useUsers from "../../users/hooks/useUsers";
 
 export default function Footer() {
   const navigate = useNavigate();
+  const { user } = useUsers();
 
   return (
     <Paper
@@ -24,6 +28,20 @@ export default function Footer() {
           icon={<StyleIcon />}
           onClick={() => navigate(ROUTES.CARDS)}
         />
+        {user?.isBusiness && (
+            <BottomNavigationAction
+              label="My cards"
+              icon={<RecentActorsIcon />}
+              onClick={() => navigate(ROUTES.MY_CARDS)}
+            />
+          )}
+          {user && (
+            <BottomNavigationAction
+              label="Favorite cards"
+              icon={<FavoriteIcon />}
+              onClick={() => navigate(ROUTES.FAV_CARDS)}
+            />
+          )}
       </BottomNavigation>
     </Paper>
   );
