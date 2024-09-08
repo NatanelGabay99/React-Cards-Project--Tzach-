b774e72243a47948d1002df65ef7ef1f16d08229
Natanel Gabay <natanelg1@gmail.com>
2024-09-08 23:32:48 +0300

    Refactor data display components and update styles

--- src/sandbox/effects/Countries.jsx
+++ src/sandbox/effects/Countries.jsx
@@ -1,44 +1,38 @@
-import { CircularProgress, TextField, Typography } from "@mui/material";
+import { Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import Country2 from "./Country2";
 
 export default function Countries() {
-  const [allCountries, setAllCountries] = useState([]);
-  const [filteredAllCountries, setFilteredAllCountries] = useState([]);
-  const [searchText, setSearchText] = useState("");
+  //יש לייבא את כל המדינות מהרסט אייפיאיי שעבדנו איתו בעבר
+  //ולהציג את התוצאה בלוג
+  const [countries, setCountries] = useState();
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
-    const getCountries = async () => {
-      let response = await fetch("https://restcountries.com/v3.1/all");
-      let data = await response.json();
-      console.log(data);
-      setAllCountries(data);
-      setFilteredAllCountries(data);
-    };
+    // const getData = async () => {
+    //   let result = await fetch("https://restcountries.com/v3.1/all");
+    //   result = await result.json();
+    //   setCountries(result);
+    // };
+    // getData();
 
-    getCountries();
-  }, []);
+    console.log("fetching....");
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCountries(data);
+      });
+  }, [refresh]);
 
-  useEffect(() => {
-    setFilteredAllCountries(
-      allCountries.filter((c) =>
-        c.name.common.toLowerCase().includes(searchText.toLowerCase())
-      )
-    );
-  }, [searchText]);
-
-  if (allCountries.length === 0) {
-    return <CircularProgress />;
-  }
   return (
     <div>
-      <TextField
-        value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-      />
-
-      {filteredAllCountries.map((country) => (
-        <Typography key={country.name.common}>{country.name.common}</Typography>
-      ))}
+      <Button onClick={() => setRefresh(!refresh)}>Refresh</Button>
+      {countries
+        ? countries.map((country, i) => {
+            return <Country2 country={country} key={JSON.stringify(country)} />;
+          })
+        : "Loading..."}
     </div>
   );
 }
