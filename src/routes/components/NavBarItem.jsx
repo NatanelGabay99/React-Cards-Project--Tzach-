934991a573903c4f72a50dc5f7d8ba144e7c4335
Natanel Gabay <natanelg1@gmail.com>
2024-09-08 22:47:51 +0300

    Refactor NavBar components and update MenuLink
    - Move the import of Button and Typography to NavBarItem component
    - Update styling of NavBarLink component to use spread operator for sx prop
    - Add MenuLink component to handle menu items in the navigation bar

--- src/routes/components/NavBarItem.jsx
+++ src/routes/components/NavBarItem.jsx
@@ -1,6 +1,6 @@
+import { Button, Typography } from "@mui/material";
 import React from "react";
 import NavBarLink from "./NavBarLink";
-import { Button, Typography } from "@mui/material";
 
 export default function NavBarItem({ to, sx, label }) {
   return (
--- src/routes/components/NavBarLink.jsx
+++ src/routes/components/NavBarLink.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function NavBarLink({ to, sx, children }) {
   return (
-    <Link to={to} style={{ textDecoration: "none", color: "#000", ...sx }}>
+    <Link to={to} style={{ ...sx, textDecoration: "none"}}>
       {children}
     </Link>
   );
