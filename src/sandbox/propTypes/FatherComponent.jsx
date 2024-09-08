992e8139f08568aaf90a364837b2782324a0cf76
Natanel Gabay <natanelg1@gmail.com>
2024-09-09 00:01:22 +0300

    Refactor component hierarchy and propTypes
    - Remove Acomponent and Bcomponent from the hierarchy
    - Add ChildComponent and FatherComponent with propTypes

---
+++ src/sandbox/propTypes/FatherComponent.jsx
@@ -0,0 +1,9 @@
+import ChildComponent from "./ChildComponent";
+
+export default function FatherComponent() {
+  return (
+    <div>
+      <ChildComponent data={"Tzach"}>Dabush</ChildComponent>
+    </div>
+  );
+}
